4a332107082479bd00d0d84c75f1a9a62c3cf8d0
Luke Watts <luke@thisis.la>
2023-04-27 22:45:15 +0200

    test: introduce validation of logger creation

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,67 +1,32 @@
-import { toJS } from 'mobx'
-import { LogflareHttpClient } from 'logflare-transport-core'
+import Logger from 'pino'
+import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
 import { getConfigurationOption } from '../config/config'
-import { Roarr } from 'roarr'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const logLevel = getConfigurationOption('REACT_APP_LOG_LEVEL', 'info')
 
-let loggerInstance: any
-let writeFn: any
-
-const USER_ID = window.crypto.randomUUID()
-
-const roarrToLogFlareMapper = (msg) => {
-  const { message, level, ...rest } = msg
-  return {
-    message,
-    metadata: {
-      user_id: USER_ID,
-      level,
-      ...rest,
-    },
+let loggerInstance: Logger.Logger
+if (getConfigurationOption('REACT_APP_LOGFLARE_KEY', '')) {
+  const logflareConfiguration = {
+    apiKey: getConfigurationOption('REACT_APP_LOGFLARE_KEY', ''),
+    sourceToken: getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', ''),
   }
-}
 
-const LOGFLARE_KEY = getConfigurationOption('REACT_APP_LOGFLARE_KEY', '')
-const LOGFLARE_SOURCE = getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', '')
-
-if (LOGFLARE_KEY) {
-  // eslint-disable-next-line no-console
-  console.log({ LOGFLARE_KEY, LOGFLARE_SOURCE })
-  const client = new LogflareHttpClient({
-    apiKey: LOGFLARE_KEY,
-    sourceToken: LOGFLARE_SOURCE,
-  })
-  writeFn = (message) => {
-    client.addLogEvent(roarrToLogFlareMapper(JSON.parse(message)))
-  }
-  loggerInstance = Roarr.child<{ error: Error }>((msg) => {
-    const message = toJS(msg)
-    return {
-      ...message,
-      level: message.context.logLevel,
-      context: {
-        env: 'local',
-        ...toJS(message.context),
-        ...(message.context.error && {
-          error: {
-            message: message.context.error.message,
-          },
-        }),
+  loggerInstance = Logger(
+    {
+      browser: {
+        transmit: {
+          send: createPinoBrowserSend(logflareConfiguration),
+        },
       },
-    }
-  })
+      level: logLevel,
+    },
+    createWriteStream(logflareConfiguration),
+  )
 } else {
-  writeFn = (message) => {
-    // eslint-disable-next-line no-console
-    console.log(JSON.parse(message))
-  }
-  loggerInstance = Roarr.child({})
+  loggerInstance = Logger({
+    browser: { asObject: false },
+    level: logLevel,
+  })
 }
-
-globalThis.ROARR.write = writeFn
-
-loggerInstance.info('Logger initialized')
 
 export const logger = loggerInstance
